4bba99004c4aa4ee4067239bbacac44420890092
Mmdreza <agpmmdreza@gmail.com>
2023-06-30 23:56:32 +0330

    add time check for class join

--- src/utils/time/index.ts
+++ src/utils/time/index.ts
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 // function that set time out for specified time
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -16,4 +18,31 @@ export function convertToDateAndTime(d: Date | string) {
   var formattedDateTime =
     formattedHour + ":" + formattedMinute + " " + formattedDate;
   return formattedDateTime;
+}
+
+function getJalaliDayIndex(day: string) {
+  // start of week is Monday
+  switch (day) {
+    case "شنبه":
+      return 6;
+    case "یکشنبه":
+      return 7;
+    case "دوشنبه":
+      return 1;
+    case "سه شنبه":
+      return 2;
+    case "چهارشنبه":
+      return 3;
+    case "پنجشنبه":
+      return 4;
+    case "جمعه":
+      return 5;
+    default:
+      return 0;
+  }
+}
+
+export function isClassJoinTime(dayNames: string[]) {
+  const mappedDayIndexes = dayNames.map((d) => getJalaliDayIndex(d));
+  return mappedDayIndexes.some((i) => i === moment("2023-07-01").isoWeekday());
 }
